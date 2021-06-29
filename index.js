af52b0ac0ab293b0dc36f76958e6ba9e446467b0
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2021-06-28 19:39:35 -0500

    pregunta#1

---
+++ index.js
@@ -0,0 +1,45 @@
+const { response } = require('express');
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000
+
+
+let conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'lab10_employees'
+});
+
+conn.connect(function (err) {
+    if (err) throw err;
+    console.log("Conexión exitosa a base de datos");
+});
+
+
+app.get("/empleados/get", function (req, res) {
+
+    conn.query("SELECT * FROM employees", function (err, results) {
+        if (err) throw err;
+
+        let employeeList = [];
+        for (var j = 0; j < results.length; j++) {
+
+            employee = {
+                'EmployeeID': results[j]['EmployeeID'],
+                'LastName': results[j]['LastName'],
+                'FirstName': results[j]['FirstName'],
+                'Title': results[j]['Title']
+            };
+            employeeList.push(employee);
+        }
+
+        res.json(employeeList);
+    });
+
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
